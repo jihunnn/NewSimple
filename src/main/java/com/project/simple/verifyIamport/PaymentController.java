23de3fa70a0bbe720a00bfadd0f2614a1c444454
Administrator <jihunnn>
2021-08-16 17:59:31 +0900

    update: payment system
    결제시스템 업데이트 중

--- src/main/java/com/project/simple/verifyIamport/PaymentController.java
+++ src/main/java/com/project/simple/verifyIamport/PaymentController.java
@@ -1,6 +1,7 @@
 package com.project.simple.verifyIamport;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.request.CancelData;
+import com.siot.IamportRestClient.response.AccessToken;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 
@@ -28,20 +30,24 @@ private IamportClient client;
 	public PaymentController() {
     	// REST API 키와 REST API secret 를 아래처럼 순서대로 입력한다.
 		this.client = new IamportClient("7287647296706666","rVuYmko5yfO9IzrEfeKSSqylUUL2BDZ3yt1UOq0mGOwooX3DbNv3e3o9YabzNbClEEgs5gPM5latje7v");
+	
 	}
 	
-
-	public void testCancelPaymentByImpUid() throws Exception {
-		//이미 취소된 거래 imp_uid
-		CancelData cancel1 = new CancelData("imp_448280090638", true);
-		//IamportResponse<Payment> cancelpayment1 = client.cancelPayment(cancel1);
-		//System.out.println(cancelpayment1.getMessage());
-	}
 		
 	@ResponseBody
 	@RequestMapping(value="/verifyIamport.do", method = RequestMethod.POST)
 	public IamportResponse<Payment> paymentByImpUid(Model model, HttpSession session, @RequestParam("imp_uid") String imp_uid ) throws IamportResponseException, IOException{			
-		System.out.println(imp_uid);	
+			
+		IamportResponse<Payment> payment = client.paymentByImpUid(imp_uid);
+		Payment paymentData = payment.getResponse();
+		BigDecimal amount = paymentData.getAmount();
+		
+		System.out.println(payment);
+		
+		
+		
+		
+		
 		return client.paymentByImpUid(imp_uid);
 	}
 
